09619a067da40d3c1fbda68ac15d8adc64ab4f01
David <david@FlatMagic.local>
2018-03-02 10:16:50 -0500

    framework completed

--- scripts.js
+++ scripts.js
@@ -1,16 +1,39 @@
 $(function(){
 	
-	$("h1").append(" pussy");
-	$("p").text("ballsy").click(function(){
-		$(this).fadeOut(2000);
-		$("h1").append("ballsy");
-		});
+	/*$( "h1" ).append( " pussy" );
+	$( "p" ).text( "ballsy" ).click(function(){
+		$( this ).fadeOut( 2000 );
+		$( "h1" ).append( "ballsy" );
+		});*/
 	
-	$(".input_button").css("margin", "200px").css("background-color", "red");
+//	$( ".input_button" ).css("margin", "20px").css("background-color", "red");
 	
-	$("button.submit").click(function(){
+	$( "button.submit" ).click(function(){
 		$("h1").css("background-color", "red");
 		});
 	
+	$( "#quote-holder" ).click(function(){
+		$( "#quote-holder > p > span" ).fadeOut( 5000, function(){
+			$( this ).fadeIn ( 5000 ) .text( "balls!");	
+		}); //this won't fade out slowly, no desire to make deferred obj so will turn main.css :hover into jquery at a later time.
+	});
 	
+	$( "#most-important" ).click(function(){
+		
+	})
 });
+
+
+/*
+
+<script>
+$( "form" ).submit(function( event ) {
+  if ( $( "input:first" ).val() === "correct" ) {
+    $( "span" ).text( "Validated..." ).show();
+    return;
+  }
+ 
+  $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
+  event.preventDefault();
+});
+</script>*/
